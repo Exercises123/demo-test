42681ad2124e24650db5a8f3b0dae2db36a18d0d
Александр Горчаков <AlVGorchakov@beeline.ru>
2022-05-27 17:10:39 +0300

    added TestClass

---
+++ src/main/java/examples/ClassEx.java
@@ -0,0 +1,25 @@
+package examples;
+//
+//public class ClassEx {
+//
+////    void testMethod() {
+////        open("https://demoqa.com/automation-practice-form");
+////    }
+////
+////    void testMethod() {
+////        open("https://demoqa.com/automation-practice-form");
+////    }
+////
+////    void testMethod() {
+////        open("https://demoqa.com/automation-practice-form");
+////    }
+////
+////    static void testMethod() {
+////        open("https://demoqa.com/automation-practice-form");
+////    }
+//
+//
+////    public static void main(String[] args) {
+////        testMethod();
+////    }
+////}
---
+++ src/test/java/webinar/TestClass.java
@@ -0,0 +1,22 @@
+package webinar;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestClass {
+
+
+
+
+    @Test
+    void fillForm() {
+        open("https://demoqa.com/automation-practice-form");
+
+        $(".mr-sm-2.form-control").sendKeys("123Name");
+        $("#lastName").sendKeys("LastName");
+        $("#userNumber").sendKeys("1231231231");
+    }
+
+}
